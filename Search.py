4c087270aa36c3b65be3002903171c1c848779ed
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-23 21:23:52 +0200

    include shit

--- Search.py
+++ Search.py
@@ -2,6 +2,6 @@ from riak import RiakClient
 
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-results = client.fulltext_search("tweets", "")
+results = client.fulltext_search("tweets", "*")
 print results
 print results['docs']
