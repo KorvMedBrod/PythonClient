04f0b587e04aac8ac0171d0cec105a109d508228
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-23 21:53:33 +0200

    syntax

--- Search.py
+++ Search.py
@@ -3,5 +3,4 @@ from riak import RiakClient
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 results = client.fulltext_search("tweets", "525322516722892802:*")
-print results
-print results['docs']
+print("Output; ",results)
