7766a8af41c324d4a8ffae3e130f04c34e2b7ae6
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-23 21:51:34 +0200

    Last time I said it works? I was kidding.

--- Search.py
+++ Search.py
@@ -2,6 +2,6 @@ from riak import RiakClient
 
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-results = client.fulltext_search("tweets", "525322516722892802")
+results = client.fulltext_search("tweets", "525322516722892802:*")
 print results
 print results['docs']
