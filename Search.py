80cb34854365922ec6520c9d11693e757bef2b87
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-23 22:29:57 +0200

    Obligatory placeholder commit message

--- Search.py
+++ Search.py
@@ -4,7 +4,8 @@ import riak
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 tweetsBucket = client.bucket('tweets')
-tweetsBucket.get("*")
+fetchedTweet = tweetsBucket.get("525322516722892802")
+print(tweetsBucket.encoded_data)
 
 #results = client.fulltext_search("tweets", "525322516722892802:*")
 #print("Output; ",results)
