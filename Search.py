fcef3c3e1f82ba2af2dd282b3d759798dff17222
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-23 22:24:40 +0200

    that coulda been bad

--- Search.py
+++ Search.py
@@ -1,6 +1,9 @@
-from riak import RiakClient
+import riak
 
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-results = client.fulltext_search("tweets", "525322516722892802:*")
-print("Output; ",results)
+tweetsBucket = client.bucket('tweets')
+tweetsBucket.get("*")
+
+#results = client.fulltext_search("tweets", "525322516722892802:*")
+#print("Output; ",results)
