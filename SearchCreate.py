23bf7bda3d7928f76db3567f2a807521a9ce989e
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-24 10:20:01 +0200

    changed names

--- SearchCreate.py
+++ SearchCreate.py
@@ -1,10 +1,10 @@
 from riak import RiakClient
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-xml_file = open('tweets.xml', 'r')
+xml_file = open('tweetsSchema.xml', 'r')
 schema_data = xml_file.read()
 print(schema_data)
-client.create_search_schema('tweets', schema_data)
+client.create_search_schema('tweetsSchema', schema_data)
 xml_file.close()
-client.create_search_index("tweets", "tweets")
+client.create_search_index("tweets", "tweetsSchema")
 print(client.list_search_indexes())
