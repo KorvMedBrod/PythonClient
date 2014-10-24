b03d00b425c6208f97c1d8442f6192a33d86d790
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-24 09:56:10 +0200

    custom search schema

--- SearchCreate.py
+++ SearchCreate.py
@@ -1,6 +1,12 @@
 from riak import RiakClient
 
-
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-client.create_search_index("tweets")
-print(client.list_search_indexes())
+xml_file = open('tweets.xml', 'r')
+schema_data = xml_file.read()
+client.create_search_schema('tweets', schema_data)
+xml_file.close()
+
+
+
+#client.create_search_index("tweets")
+#print(client.list_search_indexes())
