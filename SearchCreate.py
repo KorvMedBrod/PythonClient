37844a207f9a4a8ad715bba40ad818eb689aecf6
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-24 09:59:02 +0200

    debug

--- SearchCreate.py
+++ SearchCreate.py
@@ -3,6 +3,7 @@ from riak import RiakClient
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 xml_file = open('tweets.xml', 'r')
 schema_data = xml_file.read()
+print(schema_data)
 client.create_search_schema('tweets', schema_data)
 xml_file.close()
 
