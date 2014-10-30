559de51a9eee1fe3b830998910ab5e2fd52c01f2
Ludwig Björk <gusbjorklu@student.gu.se>
2014-10-30 15:17:57 +0100

    update

--- Search.py
+++ Search.py
@@ -3,6 +3,6 @@ import riak
 
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
-tweetsBucket = client.bucket('tweetsSchema')
+tweetsBucket = client.bucket('tweets')
 print(client.fulltext_search(tweetsBucket,"*:*"))
 #print(tweetsBucket.get("525322516722892802").encoded_data)
