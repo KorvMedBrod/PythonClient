c60f5cbdb58577e4d90bc7f86aff1a4ffa1850c1
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-03 19:37:20 +0100

    fix

---
+++ Main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+from Server import Server
+
+Server.start()
--- Server/Server.py
+++ Server/Server.py
@@ -5,36 +5,37 @@ import glob
 import logging
 import logging.handlers
 
-LOG_FILENAME = 'logs/access.out'
 
-# Set up a specific logger with our desired output level
-accessLog = logging.getLogger('MyLogger')
-accessLog.setLevel(logging.DEBUG)
+def runServer():
+    while True:
+       c, addr = s.accept()     # Establish connection with client.
+       #print 'Got connection from', addr
+       inData =  c.recv(1024)
+       accessLog.debug("inData is; %s",inData)
 
-# Add the log message handler to the logger
-handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=5)
-accessLog.addHandler(handler)
+       c.close()                # Close the connection
 
-#handeling the socket
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 8080              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-c, addr = s.accept()
+def start():
+    LOG_FILENAME = 'logs/access.out'
 
+    # Set up a specific logger with our desired output level
+    accessLog = logging.getLogger('MyLogger')
+    accessLog.setLevel(logging.DEBUG)
 
+    # Add the log message handler to the logger
+    handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=5)
+    accessLog.addHandler(handler)
 
+    #handeling the socket
+    HOST = ''                 # Symbolic name meaning all available interfaces
+    PORT = 8080              # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    c, addr = s.accept()
 
-c.sendall('connected to server')
+    c.sendall('connected to server')
 
+    accessLog.debug("Connected by %s", addr)
 
-accessLog.debug("Connected by %s", addr)
-
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   #print 'Got connection from', addr
-   inData =  c.recv(1024)
-   accessLog.debug("inData is; %s",inData)
-
-   c.close()                # Close the connection
+    runServer()
