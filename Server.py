174a41d490bd4ec30f37f2d5df686c0447022ac3
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-03 19:08:40 +0100

    Changed the logging

--- Server.py
+++ Server.py
@@ -1,8 +1,20 @@
 # Echo server program
 import socket
 from sys import exit
-#global variables
-delayTime = 1 #the delay between the packages for making the car move
+import glob
+import logging
+import logging.handlers
+
+LOG_FILENAME = 'logs/access.out'
+
+# Set up a specific logger with our desired output level
+accessLog = logging.getLogger('MyLogger')
+accessLog.setLevel(logging.DEBUG)
+
+# Add the log message handler to the logger
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=5)
+accessLog.addHandler(handler)
+
 #handeling the socket
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 8080              # Arbitrary non-privileged port
@@ -10,13 +22,19 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 c, addr = s.accept()
+
+
+
+
 c.sendall('connected to server')
-print "Connected by", addr
+
+
+accessLog.debug("Connected by", addr)
 
 while True:
    c, addr = s.accept()     # Establish connection with client.
    #print 'Got connection from', addr
    inData =  c.recv(1024)
-   print("inData is; ",inData)
+   accessLog.debug("inData is; ",inData)
 
    c.close()                # Close the connection
