220d86068c9c26de9fbec362c40bb5a7190d3545
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-05 15:15:41 +0100

    adding to database

--- Client/AddToDatabase.py
+++ Client/AddToDatabase.py
@@ -1,1 +1,8 @@
 from riak import RiakClient, RiakNode
+
+client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
+
+bucket = client.bucket_type('tweets').bucket('tweets')
+
+add = bucket.new('userName', {'name': 'asd123', 'location': "Sweden", 'created_at': "123-123-123" 'id_str':"123354123"})
+add.store()
