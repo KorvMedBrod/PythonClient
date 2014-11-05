520c0b3cebe13c2f2fa43efec25810143740d461
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-05 15:17:37 +0100

    typo

--- Client/AddToDatabase.py
+++ Client/AddToDatabase.py
@@ -4,5 +4,5 @@ client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 
 bucket = client.bucket_type('tweets').bucket('tweets')
 
-add = bucket.new('userName', {'name': 'asd123', 'location': "Sweden", 'created_at': "123-123-123" 'id_str':"123354123"})
+add = bucket.new('userName', {'name': 'asd123', 'location': "Sweden", 'created_at': "123-123-123", 'id_str': "123354123"})
 add.store()
