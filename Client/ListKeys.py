b5854bb6df85fc151702376044693487a17507f4
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-07 15:15:13 +0100

    updated bucket type

--- Client/ListKeys.py
+++ Client/ListKeys.py
@@ -3,6 +3,6 @@ from riak import RiakClient, RiakNode
 
 client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 
-bucket = client.bucket_type('tweets').bucket('tweets')
+bucket = client.bucket_type('default').bucket('tweets')
 
 print(bucket.get_keys())
