60bd22e0445568de0bc5b22eecc5423d82129ba8
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-07 15:25:47 +0100

    list keys number

--- Client/ListKeys.py
+++ Client/ListKeys.py
@@ -5,4 +5,6 @@ client = RiakClient(protocol='pbc', host='127.0.0.1', http_port=8098)
 
 bucket = client.bucket_type('default').bucket('tweets')
 
-print(bucket.get_keys())
+keys = bucket.get_keys()
+
+print(len(keys))
